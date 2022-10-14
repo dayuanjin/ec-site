1be56e77cf26f9dffcccdfd50d6d7e86d2891a00
dayuanjin <daeyoen98@gmail.com>
2022-10-14 15:10:19 +0900

    refactor: switchでpopperだし分けに修正

--- src/libs/hooks/useOpenPopper.ts
+++ src/libs/hooks/useOpenPopper.ts
@@ -1,17 +1,39 @@
 import { useState } from "react";
 
-export type HandleMouseOver = (event: React.MouseEvent<HTMLElement>) => void;
+export type HandleMouseOver = (
+  event: React.MouseEvent<HTMLElement>,
+  key: string
+) => void;
 export type HandleMouseOut = () => void;
 export type AnchorEl = HTMLElement | null;
 
 export const useOpenPopper = () => {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [popperKey, setPopperKey] = useState<string>("");
   const isOpen = Boolean(anchorEl);
-  const handleMouseOver = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleMouseOver = (
+    event: React.MouseEvent<HTMLElement>,
+    key: string
+  ) => {
+    switch (key) {
+      case "item":
+        console.log(key);
+        setPopperKey(key);
+        setAnchorEl(event.currentTarget);
+        break;
+      case "favorite":
+        setPopperKey(key);
+        setAnchorEl(event.currentTarget);
+        break;
+      case "cart":
+        setPopperKey(key);
+        setAnchorEl(event.currentTarget);
+        break;
+    }
   };
   const handleMouseOut = () => {
+    setPopperKey("");
     setAnchorEl(null);
   };
-  return { anchorEl, isOpen, handleMouseOver, handleMouseOut };
+  return { anchorEl, isOpen, handleMouseOver, handleMouseOut, popperKey };
 };
